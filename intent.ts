5992d9ae57d30ef37c2d32ba9e2871d23f577233
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-08-14 14:06:41 -0700

    Update copyright notice

--- intent.ts
+++ intent.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 MagickML
+ * Copyright 2023-present MagickML
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
